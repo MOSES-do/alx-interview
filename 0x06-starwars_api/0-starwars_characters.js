30b0cdaa93673077268d0d7ded769c992e7f13ce
Esumei Moses <archer.me24@gmail.com>
2024-09-08 18:01:55 -0700

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,7 +7,7 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err,
   const actors = JSON.parse(body).characters;
   sameOrder(actors, 0);
 });
-const sameOrder= (actors, x) => {
+const sameOrder = (actors, x) => {
   if (x === actors.length) return;
   request(actors[x], function (err, res, body) {
     if (err) throw err;
